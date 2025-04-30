469bbb9466b252e4161c0e25d091995d3e380c6b
jf.sun <jf.sun@centralsoft.com.cn>
2025-04-30 09:38:25 +0800

    doc: 数据补充

--- components/settings/BackgroundSettings.tsx
+++ components/settings/BackgroundSettings.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Card, CardContent} from "@/components/ui/card";
 import {Label} from "@/components/ui/label";
 import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
